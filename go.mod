60c4c33cc3dbf7142adbeb1603bed2d4f93c0256
Rohit Jha <rjha@godaddy.com>
2021-04-25 19:25:12 -0700

    add tests

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gofiber/fiber/v2 v2.7.1 // direct
 	github.com/klauspost/compress v1.11.13 // indirect
+	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fasthttp v1.23.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/rohitjha/go-microservice/handlers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
+func Setup() *fiber.App {
 	app := fiber.New()
 	app.Get("/", handlers.APIHandler)
-	app.Listen(":3000")
+	return app
+}
+
+func main() {
+	app := Setup()
+	log.Fatal(app.Listen(":3000"))
 }
