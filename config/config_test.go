264c13f016bcb140a3e7e772de7a4316b41fbbe3
Rohit Jha <rohit305jha@gmail.com>
2021-04-26 17:36:05 -0700

    add unit test for config

--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListProducts(t *testing.T) {
+	config, err := LoadConfig("..")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, config)
+
+	assert.Equal(t, "3001", config.Port)
+	assert.Equal(t, "./certs/localhost.crt", config.CertFileLocation)
+	assert.Equal(t, "./certs/localhost.key", config.KeyFileLocation)
+}
